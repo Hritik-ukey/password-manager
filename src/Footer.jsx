606c714b59fd5aebf1a1ab848f23d7dd37951e2d
Hritik <hritikukey840@gmail.com>
2024-10-13 10:43:03 +0530

    Improving Footer Section

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div className=' bg-gray-500 text-white font-bold text-xl text-center absolute bottom-0 w-full min-h-2'>This is the Password Manager Fo You !!!</div>
+      <div className=' bg-gray-500 text-white font-bold text-xl text-center absolute bottom-0 w-full min-h-2'>This is the Password Manager For You !!!</div>
     </>
   )
 }
